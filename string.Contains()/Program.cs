b5a95b5bb31da2f1bcc3cf73a0aa038564f74d93
MsCurly <zuleyha_98@live.com>
2021-01-16 15:52:47 +0300

    ilk gonderme

---
+++ string.Contains()/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string.Contains__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen isminizi girniz:");
+            string isim = Console.ReadLine();
+            bool isimContainA = isim.Contains("a");
+
+            if (isimContainA == false)
+
+                Console.WriteLine("İsminde 'a' harfi olmayanlar bu grupta değiller");
+            else
+                Console.WriteLine("A harfini içeriyor. \nBu gruptasınız");
+
+
+            Console.ReadLine();
+        }
+    }
+}
